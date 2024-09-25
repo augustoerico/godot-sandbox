8f67ed067274ffc6d582cba8ff66fc47848eda07
Erico Silva <augusto.ericosilva@gmail.com>
2024-09-25 12:43:20 -0300

    Project setup

---
+++ test/unit/test_calculator.gd
@@ -0,0 +1,13 @@
+extends GutTest
+
+func test_should_add_two_integers():
+	# given
+	var calculator = Calculator.new()
+	var a = 5
+	var b = 2
+	
+	# when
+	var result = calculator.add(a, b)
+	
+	# then
+	assert_eq(result, 7)
